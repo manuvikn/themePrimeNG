57e6e3659899b62f64b317a519afb8c56b06ddb6
manuvikn <manuelvictoriacode@gmail.com>
2021-10-29 18:34:05 +0200

    implementacion navbar y colores de background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './components/main.component';
+import { MainComponent } from './components/main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { PrimeNgModule } from './primeNG/primeng.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
